a942c24942b79a55c952dc6e88f56ddc001ba3ab
MrAcG <gonsalvesaquil@gmail.com>
2018-04-03 21:53:18 +0530

    menu

--- storage/framework/views/67dfb2521d975ddc3757cabac63db0707f30e516.php
+++ storage/framework/views/67dfb2521d975ddc3757cabac63db0707f30e516.php
@@ -32,17 +32,49 @@
                     </button>
 
                     <!-- Branding Image -->
-                    <a class="navbar-brand" href="<?php echo e(url('/home')); ?>">
-                       Brand The Talent
-                    </a>
+                <ul class="nav navbar-nav navbar-right">
+                    <li class="dropdown">
+                       <a href="#" class="navbar-brand"  data-toggle="dropdown" role="button" aria-expanded="false">
+                        Brand The Talent  <span class="caret"></span>
+                       </a>
+                      <ul class="dropdown-menu" role="menu">
+                                    <li>
+                                        <a href="<?php echo e(url('/profiling')); ?>">
+                                            Personality Profiling
+                                        </a>
+                                
+                                        <a href="<?php echo e(url('/subject')); ?>">
+                                            Subjects
+                                        </a>
+                                    
+                                        <a href="<?php echo e(url('/softskills')); ?>">
+                                            Soft Skills
+                                        </a>
+                                    
+                                        <a href="<?php echo e(url('/selfdev')); ?>">
+                                            Self Development Plan
+                                        </a>
+                                    
+                                        <a href="<?php echo e(url('/aptitude')); ?>">
+                                            Aptitude Test
+                                        </a>
+                                   
+                                        <a href="/jobs">
+                                            Jobs
+                                        </a>
+                                    
+                                        <a href="resume">
+                                            Resume
+                                        </a>
+                                    </li>
+                      </ul>
+                      </li>
+                   </ul>
                 </div>
 
                 <div class="collapse navbar-collapse" id="app-navbar-collapse">
                     <!-- Left Side Of Navbar -->
-                    <ul class="nav navbar-nav">
-                        &nbsp;
-                    </ul>
-
+                   
                     <!-- Right Side Of Navbar -->
                     <ul class="nav navbar-nav navbar-right">
                         <!-- Authentication Links -->
